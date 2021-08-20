82675afcff721d392ded59a37d13754ee1c550cb
Jesper Paulsen <jesper@jesper.no>
2021-08-20 13:02:37 +0200

    Init preact+vite+tailwind

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/essential', 'standard', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# Waste Collector - A Chrome Plugin
+
+This plugin collects the amount of data you are consuming. This is sent to the waste analyzer to look at your environment impact.
