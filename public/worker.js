5f9caaafcf1b3a9c14bcbbd0a89891d01e705349
Jesper Paulsen <jesper@jesper.no>
2021-08-25 13:03:54 +0200

    Basic requests monitoring

--- public/worker.js
+++ public/worker.js
@@ -1,3 +1,22 @@
-chrome.webRequest.onResponseStarted.addListener(function (details) {
+chrome.runtime.onInstalled.addListener(function (details) {
   console.log(details);
 });
+chrome.webRequest.onHeadersReceived.addListener(
+  function (details) {
+    var _a;
+    var fileSize;
+    (_a = details.responseHeaders) === null || _a === void 0
+      ? void 0
+      : _a.forEach(function (v, i, a) {
+          if (v.name.toLowerCase() == 'content-length') fileSize = v.value;
+        });
+    if (!fileSize && details.type !== 'xmlhttprequest') {
+      console.error('Unable to find file size: ' + details.type);
+      console.log(details);
+    } else {
+      console.log(details.url + ': ' + fileSize);
+    }
+  },
+  { urls: ['<all_urls>'] },
+  ['responseHeaders']
+);
--- worker/worker.ts
+++ worker/worker.ts
@@ -1,3 +1,21 @@
-chrome.webRequest.onResponseStarted.addListener((details) => {
+chrome.runtime.onInstalled.addListener((details) => {
   console.log(details);
 });
+
+chrome.webRequest.onHeadersReceived.addListener(
+  function (details) {
+    var fileSize;
+    details.responseHeaders?.forEach(function (v, i, a) {
+      if (v.name.toLowerCase() == 'content-length') fileSize = v.value;
+    });
+
+    if (!fileSize && details.type !== 'xmlhttprequest') {
+      console.error('Unable to find file size: ' + details.type);
+      console.log(details);
+    } else {
+      console.log(`${details.url}: ${fileSize}`);
+    }
+  },
+  { urls: ['<all_urls>'] },
+  ['responseHeaders']
+);
