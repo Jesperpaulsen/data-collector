9b8937250060f9d63a5d3ac24c7c576767d73ef1
Jesper Paulsen <jesper@jesper.no>
2021-10-19 12:22:48 +0200

    update usageDisplay in data presenter

---
+++ packages/data-presenter/src/components/common/Hover.tsx
@@ -0,0 +1,28 @@
+import { FunctionalComponent } from 'preact'
+import { useState } from 'preact/hooks'
+
+interface Props {
+  infoText: string
+}
+
+const Hover: FunctionalComponent<Props> = ({ infoText, children }) => {
+  const [showInfo, setShowInfo] = useState(false)
+
+  return (
+    <div
+      onMouseEnter={() => setShowInfo(true)}
+      onMouseLeave={() => setShowInfo(false)}
+      className="relative cursor-pointer">
+      <div className="rounded-full flex justify-center items-center bg-gray-500 w-6 h-6 p-1">
+        {children}
+      </div>
+      {showInfo && (
+        <div className="absolute w-96 text-sm bg-gray-700 z-50 rounded pl-4 py-2 cursor-default">
+          {infoText}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Hover
