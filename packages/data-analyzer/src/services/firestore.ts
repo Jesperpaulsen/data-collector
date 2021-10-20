36ff3e185357c150f2fa4d2dd6aef4ee0843efa6
Jesper Paulsen <jesper@jesper.no>
2021-10-20 14:50:22 +0200

    update variable name in activeUsers docs

--- packages/data-analyzer/src/services/firestore.ts
+++ packages/data-analyzer/src/services/firestore.ts
@@ -85,7 +85,7 @@ export class Firestore {
 
     await docRef.set(
       {
-        numberOfUsers: admin.firestore.FieldValue.arrayUnion(uid),
+        users: admin.firestore.FieldValue.arrayUnion(uid),
         date: startOfToday
       },
       { merge: true }
